c11b077dc20ca050931a0f30add2f1f20a94de80
90534 <90534@ALI>
2024-09-22 17:34:53 +0300

    Generic Repository #2 - Write Repository

--- Infrastructure/BlogApi.Persistance/Registiration.cs
+++ Infrastructure/BlogApi.Persistance/Registiration.cs
@@ -19,6 +19,7 @@ namespace BlogApi.Persistance
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
         }
 
     }
--- Infrastructure/BlogApi.Persistance/Repositories/WriteRepository.cs
+++ Infrastructure/BlogApi.Persistance/Repositories/WriteRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BlogApi.Application.Interfaces;
+using BlogApi.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace BlogApi.Persistance.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class,IEntityBase,new()
     {
+        private readonly DbContext _dbContext;
+        public WriteRepository(DbContext dbContext)
+        {
+            _dbContext  = dbContext;
+        }
+        private DbSet<T> Table { get => _dbContext.Set<T>(); }
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity); 
+        }
+
+        public async Task AddRangeAsync(List<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+        public async Task<T> UpdateAsync(T entity) //Update metodunun asenkron hali olmadığı için burada kendimiz asenkron hale çeviriyoruz.
+        {
+            await Task.Run(() => Table.Update(entity));
+            return entity;
+        }
+
+        public async Task HardDeleteAsync(T entity)
+        {
+            await Task.Run(() => Table.Remove(entity));
+           
+        }
+
+      
     }
 }
