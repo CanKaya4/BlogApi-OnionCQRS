d9160f1ece80f065580c239ad34507f85e4b6ebf
90534 <90534@ALI>
2024-09-22 14:25:53 +0300

    Temel Entityler oluşturuldu.

---
+++ Core/BlogApi.Domain/Entities/Category.cs
@@ -0,0 +1,27 @@
+﻿using BlogApi.Domain.Common;
+using BlogApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain.Entities
+{
+    public class Category : EntityBase, IEntityBase
+    {
+        public Category()
+        {
+
+        }
+        public Category(int parentId, string categoryname)
+        {
+            ParentId = parentId;
+            CategoryName = categoryname;
+        }
+        public required int ParentId { get; set; }
+        public required string CategoryName { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
