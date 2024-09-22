87f2ae57235e6a68efb004de4079eea41a5077a1
90534 <90534@ALI>
2024-09-22 21:59:16 +0300

    AutoMapper - AutoMapper Kütüphanesini Customize

---
+++ BlogApi.Mapper/Registiration.cs
@@ -0,0 +1,19 @@
+﻿using BlogApi.Application.Interfaces.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Mapper
+{
+    public static class Registiration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<ICustomMapper, AutoMapper.Mapper>();
+        }
+    }
+}
--- Presentation/BlogApi.Api/Program.cs
+++ Presentation/BlogApi.Api/Program.cs
@@ -1,5 +1,7 @@
 using BlogApi.Persistance;
 using BlogApi.Application;
+using BlogApi.Mapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +18,7 @@ builder.Configuration.SetBasePath(env.ContentRootPath)
 
 builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
