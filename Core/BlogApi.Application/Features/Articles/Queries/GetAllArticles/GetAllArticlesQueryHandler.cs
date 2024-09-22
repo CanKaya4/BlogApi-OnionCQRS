b0c12970fa05ffce76d0e92c02131c627c8b7f51
90534 <90534@ALI>
2024-09-22 19:44:41 +0300

    CQRS'e Giriş - Mediatr

---
+++ Core/BlogApi.Application/Features/Articles/Queries/GetAllArticles/GetAllArticlesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using BlogApi.Application.Interfaces.UnitOfWorks;
+using BlogApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Application.Features.Articles.Queries.GetAllArticles
+{
+    public class GetAllArticlesQueryHandler : IRequestHandler<GetAllArticlesQueryRequest, IList<GetAllArticlesQueryResponse>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetAllArticlesQueryHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IList<GetAllArticlesQueryResponse>> Handle(GetAllArticlesQueryRequest request, CancellationToken cancellationToken)
+        {
+            var articles = await _unitOfWork.GetReadRepository<Article>().GetAllAsync();
+            List<GetAllArticlesQueryResponse> response = new();  
+            foreach(var item in articles)
+            {
+               response.Add(new GetAllArticlesQueryResponse()
+                {
+                    Title = item.Title,
+                    CategoryId = item.CategoryId,
+                    Content = item.Content
+                });
+            }
+            return response;
+        }
+    }
+}
---
+++ Core/BlogApi.Application/Features/Articles/Queries/GetAllArticles/GetAllArticlesQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Application.Features.Articles.Queries.GetAllArticles
+{
+    public class GetAllArticlesQueryResponse
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
